386706595d80067a7c11d73fc1c55b60f1459b73
fdunn1985 <fdunn1985@gmail.com>
2019-12-12 01:16:24 -0800

    Unity tutorial game

---
+++ Jack the Giant/Assets/Scripts/Collectable Scripts/CollectableScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour {
+
+    private void OnEnable() {
+        Invoke("DestroyCollectable", 6f);
+    }
+
+    void DestroyCollectable() {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Jack the Giant/Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+
+    [SerializeField]
+    private Button musicBtn;
+
+    [SerializeField]
+    private Sprite[] musicIcons;
+
+	// Use this for initialization
+	void Start () {
+        CheckToPlayTheMusic();
+	}
+
+    void CheckToPlayTheMusic() {
+        if (GamePreferences.GetMusicState() == 1) {
+            MusicController.instance.PlayMusic(true);
+            musicBtn.image.sprite = musicIcons[1];
+        } else {
+            MusicController.instance.PlayMusic(false);
+            musicBtn.image.sprite = musicIcons[0];
+        }
+    }
+	
+    public void StartGame() {
+        GameManager.instance.gameStartedFromMainMenu = true;
+        //SceneManager.LoadScene("Gameplay");
+        SceneFader.instance.LoadLevel("Gameplay");
+    }
+
+    public void HighScoreMenu() {
+        SceneManager.LoadScene("HighscorScene");
+    }
+
+    public void OptionsMenu() {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void MusicButton() {
+        if (GamePreferences.GetMusicState() == 0) {
+            GamePreferences.SetMusicState(1);
+            MusicController.instance.PlayMusic(true);
+            musicBtn.image.sprite = musicIcons[1];
+        } else if (GamePreferences.GetMusicState() == 1) {
+            GamePreferences.SetMusicState(0);
+            MusicController.instance.PlayMusic(false);
+            musicBtn.image.sprite = musicIcons[0];
+        }
+    }
+}
